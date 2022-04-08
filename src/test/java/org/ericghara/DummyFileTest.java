85de5a33562e5ee18a53caddf2ff79ddfa8f9e12
ericghara <eric.gharakhanian@gmail.com>
2022-04-08 12:04:12 -0700

    DummyFileTest: reduced real I/O

--- src/test/java/org/ericghara/DummyFileTest.java
+++ src/test/java/org/ericghara/DummyFileTest.java
@@ -1,18 +1,20 @@
 package org.ericghara;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.io.TempDir;
 import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.mockStatic;
 
 @ExtendWith(MockitoExtension.class)
 class DummyFileTest {
@@ -27,17 +29,23 @@ class DummyFileTest {
     @TempDir
     Path tempDir;
 
-    DummyFile mockConstructor(Path path) {
-        return mock(DummyFile.class,
-                withSettings().useConstructor(path)
-                              .defaultAnswer(CALLS_REAL_METHODS) );
+    EmptyFile noWriteConstructor(Path path) {
+        Path absPath = tempDir.resolve(path);
+        // prevents file writes by mocking java.nio.file.Files class
+        try(MockedStatic<Files> mockFiles = mockStatic(Files.class) ) {
+            mockFiles.when(
+                            () -> Files.createFile(any(Path.class)))
+                    .thenReturn(absPath);
+            return new EmptyFile(absPath);
+        }
     }
 
     @Test
+    // this test actually writes to disk
     void createMakesFile() {
         Path relPath = Paths.get("aFile");
         Path filePath = tempDir.resolve(relPath);
-        var dummy = mockConstructor(filePath);
+        var dummy = new EmptyFile(filePath);
         assertTrue(Files.isRegularFile(filePath) );
     }
 
@@ -45,56 +53,51 @@ class DummyFileTest {
     void getSizeCorrectSize() {
         var expected = 1234L;
         Path relPath = Paths.get("aFile");
-        Path filePath = tempDir.resolve(relPath);
-        var dummy = mockConstructor(filePath);
-        new FileWriter(filePath).write(expected,
-                new RandomByteSupplier() );
-        assertEquals(expected, dummy.getSize() );
-    }
-
-    @Test
-    void equalsReflexiveTrue() {
-        Path relPath = Paths.get("aFile");
-        Path filePath = tempDir.resolve(relPath);
-        var dummy = new EmptyFile(filePath);
-        assertEquals(dummy, dummy);
-    }
-
-    @Test
-    void equalsNullFalse() {
-        Path relPath = Paths.get("aFile");
-        Path filePath = tempDir.resolve(relPath);
-        var dummy = new EmptyFile(filePath);
-        assertNotEquals(null, dummy);
-    }
-
-    @Test
-    void equalsSamePathTrue() throws IOException {
-        Path relPath = Paths.get("aFile");
-        Path filePath = tempDir.resolve(relPath);
-        var dummyA = new EmptyFile(filePath);
-        Files.delete(filePath);
-        var dummyB = new EmptyFile(filePath);
-        assertEquals(dummyA, dummyB);
-    }
-
-    @Test
-    void equalsDifferentPathFalse()  {
-        Path relPathA = Paths.get("aFile");
-        Path relPathB = Paths.get("bFile");
-
-        try(MockedStatic<Files> mockFiles = mockStatic(Files.class) ) {
-            mockFiles.when(() -> Files.createFile(any(Path.class)))
-                    .thenReturn(Paths.get("fake/path"));
-
-            Path filePathA = tempDir.resolve(relPathA);
-            Path filePathB = tempDir.resolve(relPathB);
-
-            var dummyA = new EmptyFile(filePathA);
-            var dummyB = new EmptyFile(filePathB);
-
-            assertNotEquals(dummyA, dummyB);
+        var dummy = noWriteConstructor(relPath);
+        try (MockedStatic<Files> mockFiles = mockStatic(Files.class)) {
+            mockFiles.when(
+                            () -> Files.size(any(Path.class)))
+                    .thenReturn(expected);
+            assertEquals(expected, dummy.getSize());
         }
     }
 
+    @Nested
+    @DisplayName("Tests of equals()")
+    class EqualsTests {
+
+        @Test
+        void equalsDifferentPathFalse()  {
+            Path relPathA = Paths.get("aFile");
+            Path relPathB = Paths.get("bFile");
+
+            var dummyA = noWriteConstructor(relPathA);
+            var dummyB = noWriteConstructor(relPathB);
+
+            assertNotEquals(dummyA, dummyB);
+        }
+
+        @Test
+        void equalsReflexiveTrue() {
+            Path relPath = Paths.get("aFile");
+            var dummy = noWriteConstructor(relPath);
+            assertEquals(dummy, dummy);
+        }
+
+        @Test
+        void equalsNullFalse() {
+            Path relPath = Paths.get("aFile");
+            var dummy = noWriteConstructor(relPath);
+            assertNotEquals(null, dummy);
+        }
+
+        @Test
+        void equalsSamePathTrue() {
+            Path relPath = Paths.get("aFile");
+            var dummyA = noWriteConstructor(relPath);
+            var dummyB = noWriteConstructor(relPath);
+            assertEquals(dummyA, dummyB);
+        }
+
+    }
 }
